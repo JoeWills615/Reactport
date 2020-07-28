05b6742c1062770946e5909374ee06e8c2c10db1
Joe Wills <JGW3Code@gmail.com>
2020-07-27 23:58:53 -0500

    Updates

---
+++ precache-manifest.5bad894fe72348027b91f658ab2a16dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be6e971747aea141d4186ab1ba8e20e",
+    "url": "/JoeWills615/Reactport/index.html"
+  },
+  {
+    "revision": "8a1accfc84736819b29f",
+    "url": "/JoeWills615/Reactport/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3aa26c47d3599e372f3",
+    "url": "/JoeWills615/Reactport/static/js/2.0d6aa5c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JoeWills615/Reactport/static/js/2.0d6aa5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1accfc84736819b29f",
+    "url": "/JoeWills615/Reactport/static/js/main.09c5c0ba.chunk.js"
+  },
+  {
+    "revision": "a86fccb6031f265fe8b4",
+    "url": "/JoeWills615/Reactport/static/js/runtime-main.58820848.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JoeWills615/Reactport/static/media/logo.5d5d9eef.svg"
+  }
+]);
